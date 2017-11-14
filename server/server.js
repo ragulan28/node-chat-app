1580b4f162c7405ab38c7f82a66b5e0b5b95f3a0
Ragulan <ragulan28@gmail.com>
2017-11-15 05:10:30 +0530

    Add connect and disconnect event hadlers

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
-const path = require('path');
-const express = require('express');
-const publicPath = path.join(__dirname, '../public');
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketIO = require("socket.io");
+
+const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
-app.listen(port, () => {
+io.on("connection", socket => {
+    console.log("new user Connected");
+
+    socket.on("disconnect", () => {
+        console.log("Disconnect from server!!");
+    });
+});
+server.listen(port, () => {
     console.log(`Server is up on port: ${port}`);
-})
+});
