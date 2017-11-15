4f962435bee2778e2845e5ebc04125836d9c3e0e
Ragulan <ragulan28@gmail.com>
2017-11-15 20:47:59 +0530

    location display v2

--- public/js/index.js
+++ public/js/index.js
@@ -13,16 +13,19 @@ socket.on('newMessage', function(message) {
     jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function(message) {
+    console.log("New Location message", message);
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">My current location</a>')
+    li.text(`${message.from}: `);
+    a.attr('href', message.url)
+    li.append(a);
+    jQuery('#messages').append(li);
+});
+
 socket.on('disconnect', () => {
     console.log("Disconnect from server!!");
 });
-
-// socket.emit('createMessage', {
-//     from: 'ranjana',
-//     text: 'Hi'
-// }, function(data) {
-//     console.log('Got it', data);
-// });
 
 jQuery('#message-form').on('submit', function(e) {
     e.preventDefault();
