f05c11f221a965dddc527de72daf05ae219adfbe
Ragulan <ragulan28@gmail.com>
2017-11-15 10:44:16 +0530

    Add form for messages and show

--- public/js/index.js
+++ public/js/index.js
@@ -5,10 +5,29 @@ socket.on('connect', () => {
 
 socket.on('newMessage', function(message) {
     console.log("New message", message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from} : ${message.text}`);
+
+    jQuery('#messages').append(li);
 });
-
-
 
 socket.on('disconnect', () => {
     console.log("Disconnect from server!!");
+});
+
+socket.emit('createMessage', {
+    from: 'ranjana',
+    text: 'Hi'
+}, function(data) {
+    console.log('Got it', data);
+});
+
+jQuery('#message-form').on('submit', function(e) {
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function() {
+
+    });
 });
