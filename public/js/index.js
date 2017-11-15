7417e503edcd8e8f7283c9a2ef409a330d1d3cf1
Ragulan <ragulan28@gmail.com>
2017-11-15 22:12:17 +0530

    Add css

--- public/js/index.js
+++ public/js/index.js
@@ -29,26 +29,33 @@ socket.on('disconnect', () => {
 
 jQuery('#message-form').on('submit', function(e) {
     e.preventDefault();
+    var messageTextBox = jQuery('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextBox.val()
     }, function() {
-
+        messageTextBox.val('');
     });
 });
 
 locationButton.on('click', function() {
     if (!navigator.geolocation) {
+        locationButton.attr('disabled', 'disabled').text('Send Location');
         return alert("Not Supported by browser");
     }
+    locationButton.attr('disabled', 'disabled').text('Sending....');
     navigator.geolocation.getCurrentPosition(
         function(position) {
+            locationButton.removeAttr('disabled').text('Send Location');
+
             socket.emit('creteLocationMessage', {
                 latitude: position.coords.latitude,
                 longitude: position.coords.longitude
             });
         },
         function() {
+            locationButton.attr('disabled', 'disabled').text('Send Location');
             alert('Unable to etch location');
         });
 });
