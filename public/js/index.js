edba993384a8bfb5975dcf6e1ece636141418934
Ragulan <ragulan28@gmail.com>
2017-11-16 17:32:25 +0530

    Add auto scroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,23 @@
 var socket = io();
 //var moment = require("./lib/moment");
 
+
+function scrollToBottom() {
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        // console.log("should Scroll");
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 var locationButton = jQuery('#send-location');
 
 socket.on('connect', function() {
@@ -18,6 +35,7 @@ socket.on('newMessage', function(message) {
 
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -32,6 +50,7 @@ socket.on('newLocationMessage', function(message) {
 
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('disconnect', () => {
